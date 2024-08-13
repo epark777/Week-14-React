c7171af0d076cc4f220ed6f1058f88eeb0b8c2f0
Elliot <parkelliot777@gmail.com>
2024-08-13 08:52:23 -0700

    day2 intro and navbar

--- 2-Day/aa14-react-router-intro/src/App.jsx
+++ 2-Day/aa14-react-router-intro/src/App.jsx
@@ -1,14 +1,29 @@
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/stocks',
+    element: <Stocks />,
+  },
+  {
+    path: '/movies',
+    element: <Movies />,
+  },
+
+])
 
 function App() {
   return (
     <div className='app'>
       <h1>App Component</h1>
-      <Home />
-      <Stocks />
-      <Movies />
+      <RouterProvider router = {router} />
     </div>
   );
 }
--- 2-Day/aa14-react-router-navigation/src/App.jsx
+++ 2-Day/aa14-react-router-navigation/src/App.jsx
@@ -1,34 +1,46 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
 
+const layout = () => {
+  return (
+    <div className='app'>
+      <h1>App Component</h1>
+      <nav className='comp nav'>Here is the nav bar</nav>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: 'stocks',
-    element: <Stocks />
-  },
-  {
-    path: 'movies',
-    element: <Movies />
-  },
-  {
-    path: '*',
-    element: <h1>Page Not Found</h1>
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'stocks',
+        element: <Stocks />
+      },
+      {
+        path: 'movies',
+        element: <Movies />
+      },
+      {
+        path: '*',
+        element: <h1>Page Not Found</h1>
+      }
+    ]
   }
 ]);
 
 function App() {
-  return (
-    <div className='app'>
-      <h1>App Component</h1>
-      <RouterProvider router={router} />
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
