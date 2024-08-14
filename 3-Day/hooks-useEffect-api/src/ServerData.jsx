6dfae8ca70cbcf800bd79f662b6f1563c2c52d25
Elliot <parkelliot777@gmail.com>
2024-08-14 13:08:20 -0700

    useEffect api

---
+++ 3-Day/hooks-useEffect-api/src/ServerData.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const ServerData = () => {
+  const [serverData, setServerData] = useState([]);
+
+  useEffect(() => {
+    const fetchFortnite = async () => {
+      const res = await fetch('https://fortnite-api.com/v2/news');
+      const { data } = await res.json();
+
+      setServerData(data.br.motds[3]);
+    };
+
+    const fetchTimeout = setTimeout(() => fetchFortnite(), 2000);
+
+    return () => clearTimeout(fetchTimeout);
+  }, [])
+
+  // return <h1>No Data To Display</h1>;
+
+  return (
+      <main>
+        {serverData.map(({id, title, body, image}) => {
+          return (
+            <div className="serverContainer" key = {id}>
+              <h1>{title}</h1>
+              <h2>{body}</h2>
+              <img src={image} alt={title} />
+            </div>
+          )
+        })}
+      </main>
+  )
+};
+
+export default ServerData;
